fb68625a962b36253df534d8af72bfd8eff6a454
Axmadjon Isaqov <67532499+axiydev@users.noreply.github.com>
2022-08-31 19:04:27 +0500

    telegramm

--- lib/pages/message/message_page.dart
+++ lib/pages/message/message_page.dart
@@ -8,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/firestore.dart';
+import 'package:intl/intl.dart';
 
 class MessagePage extends StatefulWidget {
   final UserModel? model;
@@ -45,6 +46,7 @@ class _MessagePageState extends State<MessagePage> {
             bottom: 100,
             child: FirestoreListView(
               physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               shrinkWrap: true,
               reverse: true,
               query: FirebaseFirestore.instance
@@ -56,13 +58,9 @@ class _MessagePageState extends State<MessagePage> {
               itemBuilder: (context, snapshot) {
                 final MessageModel message = MessageModel.fromJson(
                     jsonDecode(json.encode(snapshot.data())));
-                return Card(
-                  child: ListTile(
-                    title: Text(message.message),
-                    tileColor: AppConsts.myId == message.idUser
-                        ? Colors.green
-                        : Colors.blue,
-                  ),
+                return MessageWidget(
+                  message: message,
+                  isMe: AppConsts.myId == message.idUser,
                 );
               },
             ),
@@ -83,6 +81,61 @@ class _MessagePageState extends State<MessagePage> {
                   ),
                 ],
               ))
+        ],
+      ),
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  final bool? isMe;
+  const MessageWidget({
+    Key? key,
+    required this.isMe,
+    required this.message,
+  }) : super(key: key);
+
+  final MessageModel message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment:
+            isMe! ? MainAxisAlignment.end : MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (!isMe!)
+            CircleAvatar(
+              child: Image.network(
+                message.urlAvatar,
+                fit: BoxFit.cover,
+              ),
+            ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: isMe! ? Colors.blue[300] : Colors.grey[400],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  message.message,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+              Text(DateFormat.Hm()
+                  .format(DateTime.tryParse(message.createdAt)!)
+                  .toString()),
+            ],
+          ),
         ],
       ),
     );
